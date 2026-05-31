73849f557b6ecded0c3032c3052bec6394a9b6df
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 13:00:57 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class loggerPerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = loggerPerifyWorker()
+    print("Worker engine initialized.")
